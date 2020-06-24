22b219801833660c848783055352d7b62d247ac6
Kenny Vu <kennyvu@live.ca>
2020-06-24 17:43:31 -0400

    exercise 10 finished!

--- workshop/exercise-10.js
+++ workshop/exercise-10.js
@@ -2,30 +2,30 @@
 // -----------
 
 let inputData = {
-  name: 'Will Byers',
+  name: "Will Byers",
   age: 9,
-  status: 'upside down',
-  superpower1: 'can-blink-lights',
+  status: "upside down",
+  superpower1: "can-blink-lights",
   superpower2: null,
-  address1: '123 Whatever street',
-  addressCity: 'Hawkins',
-  addressState: 'Indiana',
-  addressCountry: 'United States',
-  motherName: 'Joyce Byers',
+  address1: "123 Whatever street",
+  addressCity: "Hawkins",
+  addressState: "Indiana",
+  addressCountry: "United States",
+  motherName: "Joyce Byers",
   motherAge: 35,
-  motherStatus: 'worried',
+  motherStatus: "worried",
   motherSuperpower1: null,
   motherSuperpower1: null,
-  bestFriendName: 'Mike Wheeler',
+  bestFriendName: "Mike Wheeler",
   bestFriendAge: 9,
-  bestFriendStatus: 'frenetic',
+  bestFriendStatus: "frenetic",
   bestFriendSuperpower1: null,
   bestFriendSuperpower1: null,
-  girlfriendName: 'Eleven',
+  girlfriendName: "Eleven",
   girlfriendAge: 9,
-  girlfriendStatus: 'angry',
-  girlfriendSuperpower1: 'telepathy',
-  girlfriendSuperpower1: 'multiverse portal sealing',
+  girlfriendStatus: "angry",
+  girlfriendSuperpower1: "telepathy",
+  girlfriendSuperpower2: "multiverse portal sealing",
 };
 
 // We want a function that can transform it from that shape to this shape:
@@ -79,9 +79,66 @@ let inputData = {
 // ✅ ['can-blink-lights']
 // ⛔️ ['can-blink-lights', null]
 
+//logic to handle superpowers
+const combinePowers = (power1, power2) => {
+  let allPowers = [];
+  if (power1) {
+    allPowers.push(power1);
+  }
+  if (power2) {
+    allPowers.push(power2);
+  }
+  return allPowers;
+};
+
 function transformData(data) {
   // Your code here
+  let { name, age, status } = data;
+  // //add 3 unchanged key-value pairs
+  let newData = {
+    name,
+    age,
+    status,
+  };
+  //add address object
+  newData.address = {
+    streetAddress: data.address1,
+    city: data.addressCity,
+    state: data.addressState,
+    country: data.addressCountry,
+  };
+  //add superpowers
+  newData.superpowers = combinePowers(data.superpower1, data.superpower2);
+  //add relationships
+  newData.relationships = {
+    mother: {
+      type: "mother",
+      name: data.motherName,
+      age: data.motherAge,
+      status: data.motherStatus,
+      superpowers: combinePowers(data.motherSuperpower1),
+    },
+    bestFriend: {
+      type: "best friend",
+      name: data.bestFriendName,
+      age: data.bestFriendAge,
+      status: data.bestFriendStatus,
+      superpowers: combinePowers(data.bestFriendSuperpower1),
+    },
+    girlfriend: {
+      type: "girlfriend",
+      name: data.girlfriendName,
+      age: data.girlfriendAge,
+      status: data.girlfriendStatus,
+      superpowers: combinePowers(
+        data.girlfriendSuperpower1,
+        data.girlfriendSuperpower2
+      ),
+    },
+  };
+  return newData;
 }
+console.log(inputData.girlfriendSuperpower1);
 
 // `JSON.stringify` is used to "pretty-print" the output, so that it's easy
 // to see what it looks like, and debug any problems.
